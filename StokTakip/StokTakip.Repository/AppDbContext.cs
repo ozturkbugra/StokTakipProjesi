3cfa6a8287dffc820d1248aef18c9d8984b1d667
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2025-12-28 11:45:41 +0300

    Repository katmani hazirlandi, EF Core ve AppDbContext eklendi

---
+++ StokTakip/StokTakip.Repository/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StokTakip.Core; // Core katmanını referans aldığımız için burayı görüyor
+
+namespace StokTakip.Repository
+{
+    // DbContext sınıfından miras alıyoruz (EF Core'un ana sınıfı)
+    public class AppDbContext : DbContext
+    {
+        // Constructor: Veritabanı ayarlarını (Connection String) startup'tan alabilmek için
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Veritabanında hangi tablolar olacak?
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        // İlişkileri ve ayarları detaylandırdığımız yer
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Reflection: Çalışan koddaki tüm konfigürasyonları (Ayarları) bul ve uygula.
+            // (İleride ayarları ayrı dosyalara yazarsak burası otomatik görecek)
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
